bf8beaa10e9e1414497d6150b7955b2dd2737dd7
Devin Leaman <dleaman212@gmail.com>
2017-08-12 04:06:08 -0500

    Added javadoc to WoWBattlegroup interface

--- algalona/src/main/java/org/alcha/algalona/models/wow/battlegroups/WoWBattlegroup.java
+++ algalona/src/main/java/org/alcha/algalona/models/wow/battlegroups/WoWBattlegroup.java
@@ -2,9 +2,15 @@ package org.alcha.algalona.models.wow.battlegroups;
 
 /**
  * <p>Created by Alcha on 8/8/2017.</p>
+ * Interface that represents a Battlegroup in WoW. Since there's a separate list for EU and US, each
+ * one has overlapping names. Instead of using one giant enum and then trying to figure out which
+ * belongs where, this interface is the middleman for {@link WoWUSBattlegroups} and
+ * {@link WoWEUBattlegroups}.
  */
-
 public interface WoWBattlegroup {
+    /** Returns the slug to be used for URL request fields **/
     String getSlug();
+
+    /** Returns the relative URL to be used for URL request parameters **/
     String getRelativeUrl();
 }
